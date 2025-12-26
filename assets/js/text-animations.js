1a02499b27822a70c56ec7316dd4d27b24f8eb0a
Matthew <matthew@hmongvaj.de>
2025-12-26 00:29:02 -0600

    removed float animation for tech-stack in home content.
    commented out in case future me wants to use it.

--- assets/js/text-animations.js
+++ assets/js/text-animations.js
@@ -1,19 +1,19 @@
-export function techStack(selector, delay = 0.05) {
-    document.querySelectorAll(selector).forEach(target => {
-        const chars = target.textContent.split("")
-        target.textContent = '';
+// export function techStack(selector, delay = 0.05) {
+//     document.querySelectorAll(selector).forEach(target => {
+//         const chars = target.textContent.split("")
+//         target.textContent = '';
 
-        chars.forEach((char, i) => {
-            if( char === ' ') 
-            { 
-                target.appendChild(document.createTextNode(' '));
-                return;
-            }
+//         chars.forEach((char, i) => {
+//             if( char === ' ') 
+//             { 
+//                 target.appendChild(document.createTextNode(' '));
+//                 return;
+//             }
 
-            const span = document.createElement('span');
-            span.textContent = char === ' ' ? '\u00A0' : char;
-            span.style.animationDelay = `${i * delay}s`;
-            target.appendChild(span)
-        }); 
-    });
-}
+//             const span = document.createElement('span');
+//             span.textContent = char === ' ' ? '\u00A0' : char;
+//             span.style.animationDelay = `${i * delay}s`;
+//             target.appendChild(span)
+//         }); 
+//     });
+// }
