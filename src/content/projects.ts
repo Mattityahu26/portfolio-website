9cab2d36fffed48660b085b58dea7deb4124fc7a
vang838 <vang838@uwm.edu>
2026-03-28 13:08:17 -0500

    Reordered projects in list

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -9,10 +9,24 @@ export type Project = {
 
 export const projects: Project[] = [
     {
+        title: "Resume Website",
+        description: "Self-hosted portfolio site built with Next.js, TypeScript, and Docker behind a reverse proxy.",
+        tech: ["Next.js", "TypeScript", "Tailwind CSS", "Docker"],
+        live: "https://professional.hmoobvaj.us",
+        featured: true
+    },
+    {
+        title: "IPEX-LLM",
+        description: "Performance layer on top of PyTorch that utilizes Intel's optimized mathematical operations instead of native PyTorch operations to allow better performance on Intel Hardware.",
+        tech: ["PyTorch", "Python"],
+        github: "https://github.com/intel/ipex-llm",
+        featured: true
+    },    
+    {
         title: "Proxmox VE",
-        description: "",
-        tech: [],
-        github: "",
-        featured: true,
+        description: "An open-source server management platform with a web-based UI that integrates KVM hypervisor, Linux Containers (LXC), software-defined storage, and networking functionalities on a single platform.",
+        tech: ["Rust", "C", "JavaScript", "Perl", "Makefile"],
+        github: "https://github.com/proxmox",
+        featured: true
     },
 ];
